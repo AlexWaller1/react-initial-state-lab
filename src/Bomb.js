e179c1550063a4e3eec8ddbf46e0a9a9f93cd743
Alex Waller <alex@artofalexwaller.com>
2021-04-28 21:37:40 -0400

    Done.

--- src/Bomb.js
+++ src/Bomb.js
@@ -1,1 +1,29 @@
 // your Bomb code here!
+import React from 'react';
+
+class Bomb extends React.Component {
+// put state in constuctor because instructor goes first
+    constructor(props) {
+        // since bomb timers can differ, we'll pass in a prop
+        // to our Bomb component to determine what the 
+        // starting count should be.
+        super()
+
+        this.state = {
+            secondsLeft: props.initialCount
+        }
+    }
+
+    render() {
+        // use a ternary here
+        const message = this.state.secondsLeft === 0 ? 'Boom!' : `${this.state.secondsLeft} seconds left before I go boom!`;
+        // secondsLeft is a method of state
+        return(
+           <div>{message}</div>
+        )
+    }
+
+}
+
+export default Bomb;
+// must always export
--- src/index.js
+++ src/index.js
@@ -10,3 +10,8 @@ ReactDOM.render(
   </div>,
   document.getElementById('root')
 );
+
+// both components called ReactDOM.render so we'll see them
+// when the page loads
+// it is in this parent component that we pass the 
+// the initialCount component to the Bomb component
